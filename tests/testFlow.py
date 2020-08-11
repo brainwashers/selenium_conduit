83ff638dd226d9596cde8a783a7e937ee6fbe3a0
brainwashers <oliinyk.ihor@gmail.com>
2020-08-11 11:52:56 +0300

    let it go

---
+++ tests/testFlow.py
@@ -0,0 +1,27 @@
+from pages.login import LoginPage
+from tests.testCase import TestCase
+from pages.readArticle import ReadArticle
+from pages.editArticle import EditArticle
+
+USER = {
+    'username': 'enixan25',
+    'email': 'enixan25@gmail.com',
+    'password': 'Enixan25',
+    'text': 'Edited'
+}
+
+
+class TestLoginFlow(TestCase):
+
+    def test_login_as_registered_user(self):
+        login_page = LoginPage(self.driver)
+        login_page.open()
+        login_page.login_as_registered_user(USER)
+
+    def test_read_article(self):
+        read_article = ReadArticle(self.driver)
+        read_article.open_existing_article(USER)
+
+    def test_edit_article(self):
+        edit_article = EditArticle(self.driver)
+        edit_article.edit_existing_article(USER)
